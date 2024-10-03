7550f096b1098ad021b9f20b7d15eeab913429bd
Afaq Khan <101743351+afaq-khan2000@users.noreply.github.com>
2024-10-04 01:02:39 +0500

    change

--- app.js
+++ app.js
@@ -35,7 +35,10 @@ app.post("/webhook", express.raw({ type: "application/json" }), async (req, res)
 
   try {
     // Verify Stripe signature
-    event = stripe.webhooks.constructEvent(req.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
+    event = stripe.webhooks.constructEvent(
+      req.rawBody,
+      sig,
+       process.env.STRIPE_WEBHOOK_SECRET);
   } catch (err) {
     console.error(`⚠️  Webhook signature verification failed.`, err.message);
     return res.status(400).send(`Webhook Error: ${err.message}`);
